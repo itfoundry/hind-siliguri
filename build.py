df80a5d9f38a10b35f62bdcdae3810b7da4e5fb4
Liang Hai <lianghai@gmail.com>
2015-06-20 07:01:27 +0800

    Initial commit
    Add masters from Nikhil's archive.
    Add the standard features and GOADB.

---
+++ build.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+
+import itfoundrykit as kit
+kit.confirm_version('2.1.0')
+
+# - - -
+
+family = kit.Family(
+    client    = 'Google Fonts',
+    trademark = 'Hind',
+    script    = 'Bangla',
+)
+
+family.set_masters(
+    modules = [
+        # 'kerning',
+        'mark_positioning',
+        'mark_to_mark_positioning',
+    ],
+)
+
+family.set_styles()
+
+# - - -
+
+builder = kit.Builder(family)
+
+builder.fontrevision = '0.100'
+
+builder.set_options([
+
+    'prepare_styles',   # stage i
+    'prepare_features', # stage ii
+    'compile',          # stage iii
+
+    'makeinstances', #!
+    'checkoutlines', #!
+    # 'autohint',      #!
+
+    'do_style_linking',
+    'use_os_2_version_4',
+    'prefer_typo_metrics',
+    'is_width_weight_slope_only',
+
+])
+
+builder.generate_designspace()
+builder.generate_fmndb()
+
+builder.build()
